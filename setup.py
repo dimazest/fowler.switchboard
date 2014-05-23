fb361dc47491ec6b1ceb24bf9b3a51bc0590dfb4
Dmitrijs Milajevs <dimazest@gmail.com>
2014-05-23 15:51:46 +0100

    Releasing 0.1.

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-version = '0.1'
-
-
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
@@ -22,15 +19,26 @@ class PyTest(TestCommand):
 
 setup(
     name='fowler.switchboard',
-    version=version,
-    description='',
+    version='0.1',
+    description='A wrapper around Christopher Potts\' swda.py',
     long_description='''''',
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
     keywords='',
     author='Dmitrijs Milajevs, Christopher Potts',
     author_email='dimazest@gmail.com',
-    url='',
+    url='http://www.eecs.qmul.ac.uk/~dm303/',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['fowler'],
